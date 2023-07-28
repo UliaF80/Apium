1f6e5acd16cad1a8906fa3f2c1f664108213b56a
UliaF80 <uliafedorova297@cmail.com>
2023-07-28 22:03:51 +0300

    add *

---
+++ src/test/java/ru/netology/qa/SampleTest.java
@@ -0,0 +1,56 @@
+package ru.netology.qa;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SampleTest {
+
+    private AndroidDriver driver;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("appium:deviceName", "myPhone");
+        desiredCapabilities.setCapability("appium:appPackege", "com.google.android.calculator");
+        desiredCapabilities.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void sampleTest() {
+        MobileElement el1 = (MobileElement) driver.findElementById("com.miui.calculator:id/btn_5_s");
+        el1.click();
+        MobileElement el2 = (MobileElement) driver.findElementById("com.miui.calculator:id/btn_minus_s");
+        el2.click();
+        MobileElement el3 = (MobileElement) driver.findElementById("com.miui.calculator:id/btn_3_s");
+        el3.click();
+        MobileElement el4 = (MobileElement) driver.findElementById("com.miui.calculator:id/btn_equal_s");
+        el4.click();
+        MobileElement el5 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[4]/android.widget.TextView");
+        el5.isDisplayed();
+        Assertions.assertEquals("= 2",el5.getText());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
